861875eaab888933d04d696a8062b36768b41349
VikashMaurya10 <vikashmaurya10stp@gmail.com>
2024-12-29 21:29:18 +0530

    #12 trying fix deployment issue

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 // import font
 import { APP_NAME, auth } from '@/config';
 import { geistMono, geistSans } from '@/fonts';
-import Wrapper from '@/lib/wrapper';
+import Wrapper from '@/lib/Wrapper';
 import { SessionProvider } from 'next-auth/react';
 
 // import styles
--- src/lib/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Export all hooks from index so that it can be imported from @/lib directly
- *
- */
-
-export * from './wrapper';
-export * from './utils';
-export * from './fetcher';
-export * from './local-storage';
