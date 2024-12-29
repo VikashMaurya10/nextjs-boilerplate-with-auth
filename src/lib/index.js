596457a8fea5295876cf82ccbee3b8af02852530
VikashMaurya10 <vikashmaurya10stp@gmail.com>
2024-12-29 21:32:59 +0530

    #12 fix deploy issue related rename dir

---
+++ src/lib/index.js
@@ -0,0 +1,9 @@
+/**
+ * Export all hooks from index so that it can be imported from @/lib directly
+ *
+ */
+
+export * from './Wrapper';
+export * from './utils';
+export * from './Fetcher';
+export * from './local-storage';
